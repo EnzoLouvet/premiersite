29bbea5a559b1e8741a0b73c9000dd994e2f1c0a
Louvet Enzo <enzolvpro@gmail.com>
2023-10-30 00:27:18 +0100

    Mon tout premier projet

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// J'appelle ma classe cursor
+const cursor = document.querySelector('.cursor');
+// je créer l'animation de mon cuseur
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+})
+
+document.addEventListener('click', ()=>{
+    cursor.classList.add('expand');
+
+    setTimeout(()=>{
+        cursor.classList.remove("expand");
+    }, 500);
+})
+
